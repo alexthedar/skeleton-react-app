2179831073ff3dca397e498f76fc5413c1372bdf
alexthedar <alex.castaneda@gmail.com>
2018-06-21 17:42:25 -0700

    skeleton app

---
+++ src/containers/Users.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class UsersImage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>The Users</h1>
+        <p>Users conatiner</p>
+        
+      </div>
+    );
+  }
+}
+
+export default UsersImage;
